8d90768bfa5da7060be93c086b4ffcf81b085414
badger-99 <alpeter91@gmail.com>
2025-02-27 13:24:22 +0300

    Set up sonner toasts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from '@/components/theme-provider';
+import { Toaster } from '@/components/ui/sonner';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -36,6 +37,7 @@ export default function RootLayout({
           themes={['dark', 'light', 'system']}
         >
           {children}
+          <Toaster richColors />
         </ThemeProvider>
       </body>
     </html>
